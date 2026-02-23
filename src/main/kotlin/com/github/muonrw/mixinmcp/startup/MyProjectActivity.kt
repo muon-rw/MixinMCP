6ba4b4b1e9ffac49f0f3e3b3f6eb1591f064b0e6
GitHub Action <action@github.com>
2026-02-23 22:49:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/muonrw/mixinmcp/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/muonrw/mixinmcp/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.muonrw.mixinmcp.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
